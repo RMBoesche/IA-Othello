f7db8a5da949c733add7484f129240a1acccf4ab
rodrigb <rodrigo.boesche@silvaco.com>
2023-02-09 23:19:49 +0000

    finished?

---
+++ advsearch/agent007/Readme.md
@@ -0,0 +1,23 @@
+### INF01048 turma B
+
+## Participantes
+
+Kairan Mateus Lara Morandin     00275608
+Leonardo Droves Silveira        00296968
+Pedro Koinaski de Paiva         00302515
+
+
+## Minimax com poda alpha-beta e limite de profundade
+
+O nosso algoritmo jogador de Othello foi implementado utilizando a busca MINIMAX com poda alpha-beta e limite de profundidade. O nosso limite de profundidade é variável: no início do jogo ele começa com um limite de 5 e aumenta a profundidade ao longo do desenvolvimento da partida caso identifique que a última jogada foi realizada rapidamente. Faltando 20 movimentos, ele começa a dar um "Burst", aumenta em 2 o limite de profundidade caso a jogada tenha ocorrida em menos de 0.3 segundos. Nas jogadas finais, ele procura até o fim do jogo, escolhendo uma joga ótima.
+
+
+## Heurística
+
+A nossa heurística é baseada em três fatores que têm seus pesos alterados ao longo do desenvolvimento da partida. O primeiro fator é uma tabela de custo estático de cada uma das casas do tabuleiro. O segundo fator é a mobilidade, um valor que compara o número de jogadas possíveis do agente e do oponente, buscando maximizar as nossas jogadas e minimizar as do oponente. O terceiro fator é um valor que compara o número de peças do agente e do oponente no tabuleiro. No início do jogo, o custo estático das casas do tabuleiro é o único que influencia. Com menos de 20 casas livres, o fator de mobilidade começa a ser considerado com 1/3 do peso. Com menos de 10 casas livres o custo das casas para de ser utilizado e começamos a considerar apenas o fator de mobilidade e de número de peças de cada jogador. Esse esquema teve inspiração num artigo que encontramos, o link está no fim do arquivo.
+
+
+## Referências
+
+Vaishnavi Sannidhanam, Muthukaruppan Annamalai. An Analysis of Heuristics in Othello
+Disponível em: https://courses.cs.washington.edu/courses/cse573/04au/Project/mini1/RUSSIA/Final_Paper.pdf
--- advsearch/my_agent/agent.py
+++ advsearch/my_agent/agent.py
@@ -6,8 +6,10 @@ from ..othello.board import Board
 
 MAX_COST_VALUE = 100000
 MIN_COST_VALUE = -100000
+MIN = 0
+MAX = 1
 
-_depth = 5
+max_depth = 2
 _start_time = 0
 _my_player = 0
 _enemy_player = 0
@@ -22,6 +24,7 @@ _alfa_beta_cut = 0
 
 
 def make_move(state: GameState) -> Tuple[int, int]:
+    global max_depth
     """
     Returns an Othello move
     :param state: state to make the move
@@ -33,44 +36,30 @@ def make_move(state: GameState) -> Tuple[int, int]:
         move = -1, -1
         return move
     
-    # Control the time
+    # Final Burst
+    free_tiles = state.board.piece_count['.']
+    if free_tiles < 20:
+        max_depth = 5
+    elif free_tiles < 10:
+        max_depth = 7
+
     global _start_time
     _start_time = time.time()
-    move = play_othello(state)
+
+    move = next_move(state)
+
     stop_time = time.time()
     total_time = stop_time - _start_time
 
-    # Back Depth to normal if something strange happens
-    global _depth
-    if _depth <= 0:
-        _depth = 4
-
-    # Normal Throttle
-    if total_time < 0.5:
-        _depth += 1
-    if total_time > 4.85:
-        _depth -= 1
-
-    # Final Burst
-    free_tiles = state.board.piece_count['.']
-    if free_tiles < 40:
-        if total_time < 0.3:
-            _depth += 1
-    if free_tiles < 20:
-        if total_time < 0.3:
-            _depth += 2
-    elif free_tiles < 10:
-        if total_time < 0.3:
-            _depth += 4
     
     return move
 
-def play_othello(state: GameState):
+def next_move(state: GameState):
     global _my_player
     global _enemy_player
 
     _my_player = state.player
-    _enemy_player = oposite_player(state.player)
+    _enemy_player = Board.opponent(state.player)
 
     value, move = MAX(state, state.player, (MIN_COST_VALUE,None), (MAX_COST_VALUE,None))
     return move
@@ -78,20 +67,25 @@ def play_othello(state: GameState):
 
 def MAX(state: GameState, current_player, alpha, beta, depth=0):
     if stop_search(depth):
-        value_move = evaluate_board(state), None
-        return value_move
+        move_value = calculate_move_value(state), None
+        return move_value
 
     if not state.board.has_legal_move(current_player):
-        return MIN(state, oposite_player(current_player), alpha, beta, depth + 1)
+        return MIN(state, Board.opponent(current_player), alpha, beta, depth + 1)
 
-    value_move = MIN_COST_VALUE, None
+    move_value = MIN_COST_VALUE, None
 
-    for legal_move in state.board.legal_moves(current_player):
-        value_move = choose('max',
-                            value_move,
-                            (MIN(state.next_state(legal_move), oposite_player(current_player), alpha, beta, depth + 1)[0], legal_move))
+    for legal_move in state.legal_moves():
+        legal_move_value = MIN(state.next_state(legal_move), Board.opponent(current_player), alpha, beta, depth + 1)[0]
+        legal_move_tuple = (legal_move_value, legal_move)
 
-        alpha = choose('max', alpha, value_move)
+        move_value = choose(MAX,
+                            move_value,
+                            legal_move_tuple)
+
+        alpha = choose(MAX,
+                       alpha,
+                       move_value)
 
         if (alpha[0] >= beta[0]):
             global _alfa_beta_cut
@@ -103,21 +97,25 @@ def MAX(state: GameState, current_player, alpha, beta, depth=0):
 
 def MIN(state: GameState, current_player, alpha, beta, depth=0):
     if stop_search(depth):
-        value_move = evaluate_board(state), None
-        return value_move
+        move_value = calculate_move_value(state), None
+        return move_value
     
-    if not state.board.has_legal_move(_my_player):
-        return MAX(state, _enemy_player, alpha, beta, depth + 1)
+    if not state.board.has_legal_move(current_player):
+        return MAX(state, Board.opponent(current_player), alpha, beta, depth + 1)
 
-    value_move = MAX_COST_VALUE, None
+    move_value = MAX_COST_VALUE, None
 
-    for legal_move in state.board.legal_moves(current_player):
-        value_move = choose('min',
-                            value_move,
-                            (MAX(state.next_state(legal_move), oposite_player(current_player), alpha, beta, depth + 1)[0],
-                            legal_move))
+    for legal_move in state.legal_moves():
+        legal_move_value = MAX(state.next_state(legal_move), Board.opponent(current_player), alpha, beta, depth + 1)[0]
+        legal_move_tuple = (legal_move_value, legal_move)
 
-        beta = choose('min', beta, value_move)
+        move_value = choose(MIN,
+                            move_value,
+                            legal_move_tuple)
+
+        beta = choose(MIN,
+                      beta,
+                      move_value)
         
         if (beta[0] <= alpha[0]):
             global _alfa_beta_cut
@@ -127,22 +125,22 @@ def MIN(state: GameState, current_player, alpha, beta, depth=0):
     return beta
 
 
-def evaluate_board(state: GameState):
+def calculate_move_value(state: GameState):
     free_tiles = state.board.piece_count['.']
+
     if free_tiles >= 20:
         cost = static_cost(state)
-    elif free_tiles >= 10:
-        cost = 2*static_cost(state) + mobility(state)
+
     elif free_tiles >= 0:
-        cost = mobility(state) + pieces_cost(state)
-    else:
-        cost = static_cost(state)
+        cost = static_cost(state) + 30*pieces_cost(state)
+
     return cost
 
 
 def static_cost(state: GameState):
     cost = 0
     tiles = state.board.tiles
+
     static_board_tile_cost = [
         [800, -100, 20, 20, 20, 20, -100, 800],
         [-100, -150, -15, -1, -1, -15, -150, -100],
@@ -156,22 +154,13 @@ def static_cost(state: GameState):
     for line in range(8):
         for column in range(8):
             static = static_board_tile_cost[column][line]
+
             if tiles[column][line] == _my_player:
                 cost += static
+
             if tiles[column][line] == _enemy_player:
                 cost -= static
     return cost
-
-
-def mobility(state: GameState):
-    my_mobility = len(state.board.legal_moves(_my_player))
-    enemy_mobility = len(state.board.legal_moves(_enemy_player))
-
-    if(my_mobility + enemy_mobility != 0):
-        actual_mobility = 10*((my_mobility - enemy_mobility)/(my_mobility+enemy_mobility))
-        return int(actual_mobility)
-    else:
-        return 0
 
 
 def pieces_cost(state: GameState):
@@ -185,31 +174,28 @@ def pieces_cost(state: GameState):
 def stop_search(depth):
     stop_time = time.time()
     total_time = stop_time - _start_time
-    if total_time > 4.85:
+
+    if total_time > 4.8:
         return True
 
-    if depth >= _depth:
+    if depth >= max_depth:
+        # print(max_depth)
         return True
     else:
         return False
 
 
-def oposite_player(player):
-    if player == Board.WHITE:
-        return Board.BLACK
-    else:
-        return Board.WHITE
-
-
-def choose(type: str, value1: tuple, value2: tuple) -> tuple:
-    if type == 'max':
-        if value1[0] >= value2[0]:
-            return value1
-        else:
-            return value2
-    if type == 'min':
+def choose(type: int, value1: tuple, value2: tuple) -> tuple:
+    if type == MIN:
         if value1[0] <= value2[0]:
             return value1
         else:
             return value2
+
+    if type == MAX:
+        if value1[0] >= value2[0]:
+            return value1
+        else:
+            return value2
+
     return None
